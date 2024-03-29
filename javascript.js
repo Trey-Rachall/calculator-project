fe0f860107ad2f21b34868acb0ff865cae4e11b0
Trey-Rachall <mathew.rachall@gmail.com>
2024-03-28 20:12:48 -0500

    added number class to html and created new eventListeners for number buttons and symbol buttons

--- javascript.js
+++ javascript.js
@@ -34,30 +34,49 @@ function operate() {
    } else if (operator === "-") {
       solution = subtract();
       return solution;
-   } else if (operator === "X") {
+   } else if (operator === "×") {
       solution = multiply();
       return solution;
-   } else if (operator === "/") {
+   } else if (operator === "÷") {
       solution = divide();
       return solution;
    };
 };
 
-/* setting the buttons to variable bttns */
-const bttns = document.querySelectorAll("button");
-
-/* event listener for buttons */
-bttns.forEach((button) => {
+/*selects the number buttons*/
+const numButton = document.querySelectorAll(".number");
+numButton.forEach((button) => {
    button.addEventListener("click", () => {
-      popDisplay(button.textContent);
-      
+      console.log(15);
    });
 });
+/*selects the operator buttons*/
+const operatorBttn = document.querySelectorAll(".symbol");
+operatorBttn.forEach((button) => {
+   button.addEventListener("click", () => {
+      console.log(12);
+   })
+})
+/* setting the buttons to variable bttns */
+/*const bttns = document.querySelectorAll("button");*/
+
+/* event listener for buttons */
+/*bttns.forEach((button) => {
+   button.addEventListener("click", () => {
+      if (button.textContent === "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9"){
+      let firstNumber = button.textContent;
+      console.log(firstNumber);
+      popDisplay(button.textContent);} else if (button.textContent === "+" || "-" || "×" || "/") {
+      let operator = button.textContent;
+      console.log(operator);
+      };
+      
+});
+});*/
 
 
-/*STILL NEED TO STORE THE CONTENT INTO VARIABLE*/
-function popDisplay(buttonText) {
+/*displays content to numbank*/
+/*function popDisplay(buttonText) {
    document.querySelector('.numBank p').textContent = buttonText;
-   let firstNumber = buttonText;
-   console.log(firstNumber);
-};
+   
+};*/
